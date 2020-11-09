1825ff75bb3f351967ac9ba877fb3f3ff743828d
zlatnizmaj <decakgao@gmail.com>
2020-11-09 15:17:59 +0700

    from Work

---
+++ Python-Vezba/strings-01.py
@@ -0,0 +1,7 @@
+# https://towardsdatascience.com/mastering-string-methods-in-python-456174ede911
+"""
+A string is a Python data type that’s used to represent a piece of text.
+It’s written between quotes, either double quotes or single quotes and
+can be as short as zero characters, or empty string, or as long as you wish.
+
+"""
